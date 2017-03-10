5442ddc4c43dcadd1fb50f4db961299430a4b471
Omri Mizrahi <mazmaz2k@gmail.com>
2017-03-10 15:38:46 +0200

    add database

---
+++ storage/json/Items.js
@@ -0,0 +1,5 @@
+var xmlStr = '<items><item id = "111"><name>USB 2.0 Hard Drive Adapter</name><price units = "ils">21.91</price>' +
+    '<from>China</from></item><item id = "1232"><name>5 in 1 USB Cable to Serial</name><price units = "ils">78.52</price>' +
+    '<from>Hong Kong</from></item><item id = "4334"><name>External USB Cable Adapter</name><price units = "ils">23.52</price>' +
+    '<from>China</from></item><item id = "9871"><name>Network Ethernet Cable</name><price units = "ils">15.30</price>' +
+    '<From>Thiland</From></item></items>';
---
+++ storage/json/Script.js
@@ -0,0 +1,94 @@
+var xmlPage = function() {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(xmlStr, 'text/xml');
+
+  var initModule = function() {
+   // document.getElementById("cmdLoad").addEventListener("click", fillContents, false);
+      $("#cmdLoad").click(fillContentsXML);
+      $("#cmdLoadJSON").click(fillContentsJSON);
+  }
+    var fillContentsXML = function() {
+        var str = "<h1>Our Items</h1>" +
+            "<table id = 'tblItems'>" +
+                "<tr>" +
+                    "<th class = 'number'>No.</th>" +
+                    "<th>Item's Name</th>" +
+                    "<th>Price</th>" +
+                    "<th>Send from</th>" +
+                "</tr>";
+        var i;
+        var list = doc.getElementsByTagName("item");
+        var even = false;
+        for(i = 0; i < list.length; i++)
+        {
+            str += "<tr class = ";
+            if(even === true)
+            {
+                str += "'even'";
+                even = false;
+            }
+            else
+            {
+                str += "'odd'";
+                even = true;
+            }
+
+            str += ">";
+            var n = list[i].firstChild;
+            var j;
+            str += "<td class = 'number'>" + list[i].getAttribute("id") + "</td>";
+            for(j = 0; j < list[i].childNodes.length; j++)
+            {
+                str += "<td>" + n.childNodes[0].nodeValue + "</td>";
+                n = n.nextSibling;
+            }
+            str += "</tr>";
+        }
+
+        str += "</table>";
+        $("div:first").html(str);
+
+    };
+
+    var fillContentsJSON = function() {
+      var str = "<h1>Our Items</h1>" +
+          "<table id = 'tblItems'>" +
+              "<tr>" +
+                  "<th class = 'number'>No.</th>" +
+                  "<th>Item's Name</th>" +
+                  "<th>Price</th>" +
+                  "<th>Send from</th>" +
+              "</tr>";
+
+      var even = false;
+      var itemsArr = JSON.parse(items);
+      itemsArr.items.forEach(function(item) {
+        str += "<tr class = ";
+        if(even === true)
+        {
+            str += "'even'";
+            even = false;
+        }
+        else
+        {
+            str += "'odd'";
+            even = true;
+        }
+
+        str += ">";
+        str += "<td class = 'number'>" + item.id + "</td>";
+        str += "<td>" + item.name + "</td>";
+        str += "<td>" + item.price + "</td>";
+        str += "<td>" + item.from + "</td>";
+        str += "</tr>";
+
+      });
+      str += "</table>";
+      $("div:first").html(str);
+    };
+    return {
+             initModule : initModule,
+           };
+}();
+
+$(document).ready(xmlPage.initModule);
