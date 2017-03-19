d782135e164f10b6fe98bcb19a8b53681274a178
Omri Mizrahi <mazmaz2k@gmail.com>
2017-03-19 12:08:50 +0200

    chart done

--- storage/js/CaloriesList.js
+++ storage/js/CaloriesList.js
@@ -6,46 +6,11 @@ function popitup(url) {
 }
 
 
-window.onload = function () {
 
-    var chart = new CanvasJS.Chart("chartContainer", {
-        title: {
-            text: "Fruits sold in First & Second Quarter"
-        },
 
-        data: [  //array of dataSeries     
-            { //dataSeries - first quarter
-                /*** Change type "column" to "bar", "area", "line" or "pie"***/
-                type: "column",
-                name: "First Quarter",
-                dataPoints: [
-                    { label: "banana", y: 18 },
-                    { label: "orange", y: 29 },
-                    { label: "apple", y: 40 },
-                    { label: "mango", y: 34 },
-                    { label: "grape", y: 24 }
-                ]
-            },
-            { //dataSeries - second quarter
-
-                type: "column",
-                name: "Second Quarter",
-                dataPoints: [
-                    { label: "banana", y: 23 },
-                    { label: "orange", y: 33 },
-                    { label: "apple", y: 48 },
-                    { label: "mango", y: 37 },
-                    { label: "grape", y: 20 }
-                ]
-            }
-        ]
-    });
-
-    chart.render();
-}
-
+var listArr;
 var caloriesList = function () {
-    var listArr = [];
+
 
     var initModule = function () {
         $("#cmdAdd").click(add);
@@ -91,8 +56,7 @@ var caloriesList = function () {
             var date = $("#txtDate").val();
             var item = { id: date, quantity: quantity };
             storageAPI.save("Calories", item);
-            listArr.push([date, quantity]);
-            alert(listArr[0]);
+
             fillTable();
         }
 
@@ -115,7 +79,7 @@ var caloriesList = function () {
 
     return {
         initModule: initModule,
-        listArr: listArr,
+
     };
 }();
 
@@ -123,3 +87,119 @@ $(document).ready(caloriesList.initModule);
 
 
 
+window.onload = function () {
+    listArr = storageAPI.getAll("Calories");
+    for (var i = 0; i < listArr.length; i++) {
+        listArr[i].id = new Date(listArr[i].id);
+        listArr[i].quantity = parseInt(listArr[i].quantity);
+    }
+
+    function compare1(val1, val2) {
+        if (val1.id > val2.id)
+            return 1;
+        else if (val1.id < val2.id)
+            return -1;
+        return 0;
+    }
+    var sortTxt = function (arr) {
+        arr.sort(compare1);
+
+    }
+
+    sortTxt(listArr);
+
+
+    /*
+    
+        var chart = new CanvasJS.Chart("chartContainer", {
+    
+            title:
+            {
+                text: "last week chart"
+            },
+    
+            axisY:
+            {
+                title: "amount of calories",
+            },
+    
+            data:
+            [
+                {
+                    type: "column",
+                    dataPoints:
+                    [
+                        { label: "sunday", y: listArr[0].quantity },
+                        { label: "monday", y: listArr[1].quantity },
+                        { label: "tuesday", y: listArr[2].quantity },
+                        { label: "wednesday", y: listArr[3].quantity },
+                        { label: "thursday", y: listArr[4].quantity },
+                        { label: "friday", y: listArr[5].quantity },
+                        { label: "saturday", y: listArr[6].quantity },
+                    ]
+                }
+            ]
+    
+        });
+    
+        chart.render();
+        console.log(listArr);
+    */
+    var average = function () {
+        var sum = 0;
+        for (var i = 0; i << listArr.length; i++) {
+            sum += listArr[i].quantity;
+        }
+        return sum / (listArr.length);
+    }
+    var showgh = function () {
+        var a = [];
+        for (var i = 0; i < listArr.length; i++) {
+            if (i % 5 != 0)
+                a.push({ x: listArr[i].id, y: listArr[i].quantity, indexLabel: listArr[i].quantity.toString() });
+            else
+                a.push({ x: listArr[i].id, y: listArr[i].quantity, indexLabel: "" });
+
+        }
+        return a;
+    }
+
+    var chart = new CanvasJS.Chart("chartContainer",
+        {
+            title: {
+                text: "Growth of calories "
+            },
+            animationEnabled: true,
+            axisX: {
+                valueFormatString: "DD-MMM-YYYY",
+
+                intervalType: "date",
+                labelAngle: -50,
+                labelFontColor: "rgb(0,75,141)"
+            },
+            axisY: {
+                title: "amount of calories",
+                interlacedColor: "#F0FFFF",
+                tickColor: "azure",
+                titleFontColor: "rgb(0,75,141)",
+                valueFormatString: "#.C",
+                interval: 1000
+
+            },
+            data: [
+                {
+                    indexLabelFontColor: "darkSlateGray",
+                    name: 'views',
+                    type: "area",
+                    color: "rgba(0,75,141,0.7)",
+                    markerSize: 10,
+                    dataPoints: showgh()
+                }
+
+            ]
+        });
+    chart.render();
+
+
+
+} 
